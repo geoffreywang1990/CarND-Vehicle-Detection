24b01e05e1e6177919c42d6f80cf8a5193e6d21f
Yujun Wang <yujun@Yujuns-MacBook-Pro.local>
2018-06-21 09:19:34 -0700

    tuning

--- utils.py
+++ utils.py
@@ -5,13 +5,12 @@ import cv2
 import numpy as np
 
 
-def get_hog_features(img, orient, pix_per_cell, cell_per_block, feature_vec=True):
-        features, hog_image = hog(img, orientations=orient, 
+def get_hog_features(img, orient, pix_per_cell, cell_per_block,viz = False, feature_vec=True):
+      return  hog(img, orientations=orient, 
                                   pixels_per_cell=(pix_per_cell, pix_per_cell),
                                   cells_per_block=(cell_per_block, cell_per_block), 
                                   transform_sqrt=False, 
-                                  visualise=True, feature_vector=feature_vec)
-        return features, hog_image
+                                  visualise=viz, feature_vector=feature_vec)
 
 def extract_features(imgs,  orient=9, pix_per_cell=8, cell_per_block=2):
     # Create a list to append feature vectors to
@@ -19,17 +18,17 @@ def extract_features(imgs,  orient=9, pix_per_cell=8, cell_per_block=2):
     # Iterate through the list of images
     for file in imgs:
         # Read in each one by one
-        image = mpimg.imread(file)
-        feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2LUV)
+        image = cv2.imread(file).astype(np.float32)/255
+        feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2YUV)
         hog_features = []
         for channel in range(feature_image.shape[2]):
-            hog_f,_= get_hog_features(feature_image[:,:,channel], 
+            hog_f= get_hog_features(feature_image[:,:,channel], 
                                 orient, pix_per_cell, cell_per_block, 
                                  feature_vec=True)
             hog_features.append(hog_f)
         
         hog_features = np.ravel(hog_features)
-        # Append the new feature vector to the features list
+        # Append the new feature vector to the features listn
         features.append(hog_features)
     # Return list of feature vectors
     return features
@@ -47,15 +46,11 @@ def find_cars(img, ystart, ystop, scale, svc, orient,
     # rescale image if other than 1.0 scale
     if scale != 1:
         imshape = ctrans_tosearch.shape
-        ctrans_tosearch = cv2.resize(ctrans_tosearch, (int(imshape[1]//scale), int(imshape[0]//scale)))
+        ctrans_tosearch = cv2.resize(ctrans_tosearch, None, fx = 1/scale,fy = 1/scale)
     
-    ch1 = ctrans_tosearch[:,:,0]
-    ch2 = ctrans_tosearch[:,:,1]
-    ch3 = ctrans_tosearch[:,:,2]
-
     # Define blocks and steps as above
-    nxblocks = (ch1.shape[1] // pix_per_cell)+1  #-1
-    nyblocks = (ch1.shape[0] // pix_per_cell)+1  #-1 
+    nxblocks = (ctrans_tosearch[:,:,0].shape[1] // pix_per_cell)+1  #-1
+    nyblocks = (ctrans_tosearch[:,:,0].shape[0] // pix_per_cell)+1  #-1 
     nfeat_per_block = orient*cell_per_block**2
     # 64 was the orginal sampling rate, with 8 cells and 8 pix per cell
     window = 64
@@ -65,18 +60,18 @@ def find_cars(img, ystart, ystop, scale, svc, orient,
     nysteps = (nyblocks - nblocks_per_window) // cells_per_step
     
     # Compute individual channel HOG features for the entire image
-    hog1,_ = get_hog_features(ch1, orient, pix_per_cell, cell_per_block, feature_vec=False)   
-    hog2,_ = get_hog_features(ch2, orient, pix_per_cell, cell_per_block, feature_vec=False)
-    hog3,_ = get_hog_features(ch3, orient, pix_per_cell, cell_per_block, feature_vec=False)
+    hog = [];
+    for i in range(0,3):
+        hog.append(get_hog_features(ctrans_tosearch[:,:,i], orient, pix_per_cell, cell_per_block, feature_vec=False))
     
     for xb in range(nxsteps):
         for yb in range(nysteps):
             ypos = yb*cells_per_step
             xpos = xb*cells_per_step
             # Extract HOG for this patch
-            hog_feat1 = hog1[ypos:ypos+nblocks_per_window, xpos:xpos+nblocks_per_window].ravel()
-            hog_feat2 = hog2[ypos:ypos+nblocks_per_window, xpos:xpos+nblocks_per_window].ravel() 
-            hog_feat3 = hog3[ypos:ypos+nblocks_per_window, xpos:xpos+nblocks_per_window].ravel() 
+            hog_feat1 = hog[0][ypos:ypos+nblocks_per_window, xpos:xpos+nblocks_per_window].ravel()
+            hog_feat2 = hog[1][ypos:ypos+nblocks_per_window, xpos:xpos+nblocks_per_window].ravel() 
+            hog_feat3 = hog[2][ypos:ypos+nblocks_per_window, xpos:xpos+nblocks_per_window].ravel() 
             hog_features = np.hstack((hog_feat1, hog_feat2, hog_feat3))
 
             xleft = xpos*pix_per_cell
@@ -146,47 +141,25 @@ def draw_labeled_bboxes(img, labels):
 
 
 
-def process_frame(img):
-
-    rectangles = []
-    orient = 11
-    pix_per_cell = 16
-    cell_per_block = 2
-    search_areas_list =[ [400 ,464 ,1.0], [416 ,480 ,1.0], [400 ,496 ,1.5], [432 ,528 ,1.5], [400 ,528 ,2.0], [432 ,560 ,2.0], [400 ,596 ,3.5], [464 ,660 ,3.5] ]
-    for search_area in search_areas_list:
-        rectangles.append(find_cars(img,*search_area , svc, orient, pix_per_cell, cell_per_block))
-
-    rectangles = [item for sublist in rectangles for item in sublist]
-
-    heatmap_img = np.zeros_like(img[:,:,0])
-    heatmap_img = add_heat(heatmap_img, rectangles)
-    heatmap_img = apply_threshold(heatmap_img, 1)
-    labels = label(heatmap_img)
-    draw_img, rects = draw_labeled_bboxes(np.copy(img), labels)
-    return draw_im
-    
-    
-    
-    
-    # Define a class to store data from video
 class Vehicle_Detect():
-    def __init__(self):
+    def __init__(self,svc,orient,pix_per_cell,cell_per_block):
         # history of rectangles previous n frames
         self.prev_rects = [] 
+        self.svc = svc
+        self.orient = orient
+        self.pix_per_cell = pix_per_cell
+        self.cell_per_block = cell_per_block
         
     def add_rects(self, rects):
         self.prev_rects.append(rects)
         if len(self.prev_rects) > 15:
             self.prev_rects = self.prev_rects[len(self.prev_rects)-15:]
     
-    def process_frame(img):
+    def process_frame(self,img):
         rectangles = []
-        orient = 11
-        pix_per_cell = 16
-        cell_per_block = 2
         search_areas_list =[ [400 ,464 ,1.0], [416 ,480 ,1.0], [400 ,496 ,1.5], [432 ,528 ,1.5], [400 ,528 ,2.0], [432 ,560 ,2.0], [400 ,596 ,3.5], [464 ,660 ,3.5] ]
         for search_area in search_areas_list:
-            rectangles.append(find_cars(img,*search_area , svc, orient, pix_per_cell, cell_per_block))
+            rectangles.append(find_cars(img,*search_area , self.svc, self.orient, self.pix_per_cell, self.cell_per_block))
         rectangles = [item for sublist in rectangles for item in sublist]
 
         if len(rectangles) > 0:
@@ -194,7 +167,7 @@ class Vehicle_Detect():
         heatmap_img = np.zeros_like(img[:,:,0])
         for rect_set in self.prev_rects:
             heatmap_img = add_heat(heatmap_img, rect_set)
-        heatmap_img = apply_threshold(heatmap_img, 1 + len(det.prev_rects)//2)
+        heatmap_img = apply_threshold(heatmap_img, 1 + len(self.prev_rects)//2)
      
         labels = label(heatmap_img)
         draw_img, rect = draw_labeled_bboxes(np.copy(img), labels)
